0689ebacd3f9461bd141fc1ef39693b74f876611
subhammodak <modaksubham866@gmail.com>
2026-08-08 21:36:22 +0530

    chore: add commented-out debug middleware for request logging in server.js

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ import { generateSummary } from "./generate-db-summary.js";
 import SetWebhookSubscription from "./helpers/SetWebhookSubscription.js";
 
 const app = express();
+
+// app.use((req, res, next) => {
+//     console.log(`[DEBUG] API called: ${req.method} ${req.originalUrl}`);
+//     next();
+// });
+
 app.use(cors({
     origin: "*",
     credentials: true
